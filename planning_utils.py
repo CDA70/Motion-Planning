63e53c0a8aa4df464342fd9bee736d93913e6acc
CDA <daemschris@gmail.com>
2018-03-26 18:35:48 +0200

    deleted bresenham

--- planning_utils.py
+++ planning_utils.py
@@ -1,9 +1,6 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
-#from bresenham import bresenham 
-#from scipy.spatial import Voronoi, voronoi_plot_2d
-
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -51,97 +48,6 @@ def create_grid(data, drone_altitude, safety_distance):
             grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
 
     return grid, int(north_min), int(east_min)
-
-
-# def create_grid_and_edges(data, drone_altitude, safety_distance):
-#     """
-#     Returns a grid representation of a 2D configuration space
-#     along with voronoi graph edges given obstacle data and the drone altitude.
-#     """
-#     print('start create grid and edges')
-#     # minimum and maximum north coordinates
-#     north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
-#     north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
-#     print('north min: ', north_min)
-#     print('north max: ', north_max)
-
-#     # minimum and maximum east coordinates
-#     east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
-#     east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
-#     print('east min: ', east_min)
-#     print('east max: ', east_max)
-
-#     # given the minimum and maximum coordinates we can
-#     # calculate the size of the grid.
-#     north_size = int(np.ceil((north_max - north_min + 1)))
-#     east_size = int(np.ceil((east_max - east_min + 1)))
-#     print('north grid size: ', north_size)
-#     print('east grid size: ', east_size)
-
-#     # Initialize an empty grid
-#     grid = np.zeros((north_size, east_size))
-
-#     #center offset fro grid
-#     north_min_center = np.min(data[:,0])
-#     east_min_center = np.min(data[:,1])
-
-#     #define a list to hold Voronoi points
-#     points = []
-#     #populate the grid with obstacles
-#     for i in range(data.shape[0]):
-
-#         north, east, alt, d_north, d_east, d_alt = data[i, :]
-#         #print('north: ', north)
-#         #print('east: ', east)
-#         #print('alt: ', alt)
-#         #print('d_north: ', d_north)
-#         #print('d_east: ', d_east)
-#         #print('d_alt: ', d_alt)
-        
-#         if alt + d_alt + safety_distance > drone_altitude:
-#             obstacle = [
-#                 int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
-#                 int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
-#                 int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
-#                 int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
-#             ]
-#             grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
-
-#             # add center of obstacles to points list
-#             points.append([north - north_min, east - east_min])
-
-#     # location of obstacle centers
-#     graph = Voronoi(points)
-
-#     #check each edge from graph.ridge_vertices for collision
-#     edges = []
-#     for v in graph.ridge_vertices:
-#         p1 = graph.vertices[v[0]]
-#         p2 = graph.vertices[v[1]]
-#         cells = list(bresenham(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1])))
-#         hit = False
-
-#         for c in cells:
-#             # First check if we're off the map
-#             if np.amin(c) < 0 or c[0] >= grid.shape[0] or c[1] >= grid.shape[1]:
-#                 hit = True
-#                 break
-#             # Next check if we're in collision
-#             if grid[c[0], c[1]] == 1:
-#                 hit = True
-#                 break
-
-#         # If the edge does not hit on obstacle
-#         # add it to the list
-#         if not hit:
-#             # array to tuple for future graph creation step)
-#             p1 = (p1[0], p1[1])
-#             p2 = (p2[0], p2[1])
-#             edges.append((p1, p2))
-
-#     print('end create grid and edges')        
-
-#     return grid, edges, int(north_min), int(east_min)
 
 
 def collinearity(p1,p2,p3, epsilon=1e-6):
